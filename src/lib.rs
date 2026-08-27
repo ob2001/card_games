a854eee653b8a946cc880b345aad2cfffbc357d3
ob2001 <butlero2001@outlook.com>
2026-08-26 22:46:51 -0400

    Add some new modules and traits, and include more in prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 pub mod card;
 pub mod deck;
 pub mod hand;
-pub mod game;
 pub mod player;
 pub mod stack;
 pub mod tableau;
 
-pub trait PlayableTo {
-    fn play_to(&mut self, card: card::CardSuitRank);
+pub mod game;
+pub mod ui;
+
+pub trait PlayableTo<CardSet: card::Card> {
+    fn play_to(&mut self, card: CardSet);
+}
+
+pub trait Flippable {
+    fn flip(&mut self);
+    fn flip_face_up(&mut self);
+    fn flip_face_down(&mut self);
+}
+
+pub mod prelude {
+    pub use std::fmt::{Debug, Display};
+    pub use crate::{
+        PlayableTo, Flippable,
+        card::{Card, FrenchCard, ItalianCard},
+        stack::StackVariant,
+    };
 }
