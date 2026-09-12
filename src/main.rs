d1dbb1a6911dbaaf948132bc160f662aa198d40e
ob2001 <butlero2001@outlook.com>
2026-09-11 23:46:07 -0400

    Only set RUST_BACKTRACE in debugging mode

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 use card_games::game::{klondike::KlondikeGame, canfield::Canfield};
 
 fn main() {
-    unsafe { std::env::set_var("RUST_BACKTRACE", "1"); }
+    if cfg!(debug_assertions) {
+        unsafe { std::env::set_var("RUST_BACKTRACE", "1"); }
+    }
+
     let mut game = KlondikeGame::new();
     game.init();
     let _ = game.run_game();
